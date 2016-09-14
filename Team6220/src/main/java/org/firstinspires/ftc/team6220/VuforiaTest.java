741ae68667e6250a913c71490bf109200bee14a1
Miyu Reus <miyukireus@outlook.com>
2016-09-14 02:27:27 -0700

    Added a test op mode that reads the x euler rotation value of the image realative to the camera, in radians.

---
+++ Team6220/src/main/java/org/firstinspires/ftc/team6220/VuforiaTest.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.team6220;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerActivity;
+import org.firstinspires.ftc.robotcontroller.internal.VuforiaTracker;
+
+import java.util.HashMap;
+
+@TeleOp(name="Vuforia Test", group="Tests")
+public class VuforiaTest extends LinearOpMode
+{
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        //set up vuforia
+        VuforiaTracker Tracker = FtcRobotControllerActivity.getVuforia();
+        Tracker.addTrackables("BeaconImages.xml");
+        Tracker.initVuforia();
+
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+            //index data by tracked object
+            HashMap<String, double[]> data = Tracker.getVuforiaData();
+
+            //
+            if(data.containsKey("lego"))
+            {
+                try
+                {
+                    telemetry.addData("|Lego| rX:", data.get("lego")[0]);
+                }
+                catch(Exception e)
+                {
+                    telemetry.addData("|Lego| rX:", "not found");
+                }
+
+            }
+
+            telemetry.update();
+        }
+
+        try {
+            Tracker.destroy();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
