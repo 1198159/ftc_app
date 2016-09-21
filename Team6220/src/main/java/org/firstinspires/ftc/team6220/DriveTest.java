f8dbde5709d58dba732fc264df1f8db7c4885d62
Miyu Reus <miyukireus@outlook.com>
2016-09-20 20:49:51 -0700

    Added comments to test op mode.

--- Team6220/src/main/java/org/firstinspires/ftc/team6220/DriveTest.java
+++ Team6220/src/main/java/org/firstinspires/ftc/team6220/DriveTest.java
@@ -6,14 +6,15 @@ package org.firstinspires.ftc.team6220;
 
 public class DriveTest extends MasterOpMode
 {
+
     @Override
     public void runOpMode() throws InterruptedException
     {
         while (opModeIsActive())
         {
-            drive.moveRobot(gamepad1.right_stick_x,
-                            gamepad1.right_stick_y,
-                            gamepad1. left_stick_x );
+            drive.moveRobot(gamepad1.right_stick_x,   //local x motion power
+                            gamepad1.right_stick_y,   //local y motion power
+                            gamepad1. left_stick_x ); //rotation power
         }
     }
 }
