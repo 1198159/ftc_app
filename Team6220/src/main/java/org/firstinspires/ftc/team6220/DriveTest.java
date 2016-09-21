6e2770f9823938fc914ecd059097f8c6b865859e
DESKTOP-S4GK5P3\Colew <colewelch151@gmail.com>
2016-09-20 20:26:57 -0700

    Adjustments to make 6220 Drive Test operational.

--- Team6220/src/main/java/org/firstinspires/ftc/team6220/DriveTest.java
+++ Team6220/src/main/java/org/firstinspires/ftc/team6220/DriveTest.java
@@ -1,9 +1,11 @@
 package org.firstinspires.ftc.team6220;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
 /*
     Test op mode that drives the robot with motor powers.
 */
-
+@TeleOp(name="6220 Test Drive", group="6220")
 public class DriveTest extends MasterOpMode
 {
     @Override
--- Team6220/src/main/java/org/firstinspires/ftc/team6220/MasterOpmode.java
+++ Team6220/src/main/java/org/firstinspires/ftc/team6220/MasterOpmode.java
@@ -12,18 +12,18 @@ abstract public class MasterOpMode extends LinearOpMode
 {
     //Java Enums cannot act as integer indices as they do in other languages
     //To maintain compatabliity with with a FOR loop, we
-    //TODO find is java supports a order-independent loop for each member of an array
+    //TODO find if java supports an order-independent loop for each member of an array
     //e.g. in python that would be:   for assembly in driveAssemblies:
     //                                    assembly.doStuff()
     public final int FRONT_RIGHT = 0;
-    public final int FRONT_LEFT = 0;
-    public final int BACK_LEFT = 0;
-    public final int BACK_RIGHT = 0;
+    public final int FRONT_LEFT = 1;
+    public final int BACK_LEFT = 2;
+    public final int BACK_RIGHT = 3;
 
 
     //our robot uses an omni drive, so our motors are positioned at 45 degree angles to normal positions.
     //TODO update drive assembly positions
-    //                                               mtr,                  x,   y,   rot,  gear, radius
+    //                                               mtr,                       x,   y,   rot,  gear, radius
     DriveAssembly[] driveAssemblies = { new DriveAssembly(null,new Transform2D( 1.0, 1.0, 135), 1.0, 0.1016),
                                         new DriveAssembly(null,new Transform2D(-1.0, 1.0, 225), 1.0, 0.1016),
                                         new DriveAssembly(null,new Transform2D(-1.0,-1.0, 315), 1.0, 0.1016),
