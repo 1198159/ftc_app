5c377730a96fcf82c229e6353b5937f70ac2d8fc
Dryw Wade <drywdude@yahoo.com>
2016-10-12 11:55:11 -0700

    Added drive code to location test

--- Team8923/src/main/java/org/firstinspires/ftc/team8923/TestLocation.java
+++ Team8923/src/main/java/org/firstinspires/ftc/team8923/TestLocation.java
@@ -1,11 +1,11 @@
 package org.firstinspires.ftc.team8923;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 /*
- * Used to test update location method
+ * Used to test and debug autonomous methods
  */
-@Autonomous(name = "Location Test", group = "Tests")
+@TeleOp(name = "Location Test", group = "Tests")
 public class TestLocation extends MasterAutonomous
 {
     @Override
@@ -17,6 +17,8 @@ public class TestLocation extends MasterAutonomous
 
         while(opModeIsActive())
         {
+            driveMecanumTeleOp();
+
             updateRobotLocation();
 
             telemetry.addData("X", robotX);
@@ -26,5 +28,17 @@ public class TestLocation extends MasterAutonomous
             telemetry.update();
             idle();
         }
+    }
+
+    void driveMecanumTeleOp()
+    {
+        double y = -gamepad1.left_stick_y; // Y axis is negative when up
+        double x = gamepad1.left_stick_x;
+
+        double angle = Math.atan2(y, x);
+        double power = calculateDistance(x, y);
+        double turnPower = -gamepad1.right_stick_x; // Fix for clockwise being a negative rotation
+
+        driveMecanum(angle, power, turnPower);
     }
 }
