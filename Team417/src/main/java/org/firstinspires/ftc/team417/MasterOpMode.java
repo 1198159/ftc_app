2adac9c719e1453e3ab29df5418d1b60826cdee4
Madeline <maddynnguyen@gmail.com>
2016-11-12 18:11:27 -0800

    Added scissor lift motor and scissor lift switch

--- Team417/src/main/java/org/firstinspires/ftc/team417/MasterOpMode.java
+++ Team417/src/main/java/org/firstinspires/ftc/team417/MasterOpMode.java
@@ -7,6 +7,9 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DeviceInterfaceModule;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DigitalChannelController;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.vuforia.Image;
 
@@ -20,13 +23,18 @@ abstract public class MasterOpMode extends LinearOpMode
     DcMotor motorBackLeft = null;
     DcMotor motorFrontRight = null;
     DcMotor motorBackRight = null;
+    DcMotor motorLift = null;
     BNO055IMU imu;
     Orientation angles;
+    DeviceInterfaceModule dim;                  // Device Object
+    DigitalChannel digIn;                // Device Object
+
+
 
     private ElapsedTime runtime = new ElapsedTime();
 
     // Declare constants
-    static final double COUNTS_PER_MOTOR_REV = 1440;    // eg: TETRIX Motor Encoder
+    static final double COUNTS_PER_MOTOR_REV = 1120;    // eg: TETRIX Motor Encoder
     static final double DRIVE_GEAR_REDUCTION = 1.0;     // This is < 1.0 if geared UP
     // static final double DRIVE_GEAR_REDUCTION = 0.25;     // This is < 1.0 if geared UP
     static final double WHEEL_DIAMETER_INCHES = 6.0;     // For figuring circumference
@@ -37,19 +45,26 @@ abstract public class MasterOpMode extends LinearOpMode
 
 
     public void initializeHardware()
-    {/*
+    {
 
         // Initialize motors to be the hardware motors
         motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
         motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
         motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
         motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+        motorLift = hardwareMap.dcMotor.get("motorLift");
+
+        // get a reference to a Modern Robotics DIM, and IO channels.
+        dim = hardwareMap.get(DeviceInterfaceModule.class, "dim");   //  Use generic form of device mapping
+        digIn  = hardwareMap.get(DigitalChannel.class, "digin");     //  Use generic form of device mapping
+        digIn.setMode(DigitalChannelController.Mode.INPUT);          // Set the direction of each channel
 
         // run to position mode
         motorFrontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         motorFrontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         motorBackLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         motorBackRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motorLift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
         motorFrontRight.setDirection(DcMotor.Direction.REVERSE);
         motorBackRight.setDirection(DcMotor.Direction.REVERSE);
@@ -58,6 +73,7 @@ abstract public class MasterOpMode extends LinearOpMode
         motorFrontRight.setPower(0);
         motorBackLeft.setPower(0);
         motorBackRight.setPower(0);
+        motorLift.setPower(0);
 
         // Set up the parameters with which we will use our IMU. Note that integration
         // algorithm here just reports accelerations to the logcat log; it doesn't actually
@@ -80,6 +96,6 @@ abstract public class MasterOpMode extends LinearOpMode
             sleep(100);
             angle = imu.getAngularOrientation().firstAngle;
 
-        }*/
+        }
     }
 }
--- Team417/src/main/java/org/firstinspires/ftc/team417/MasterTeleOp.java
+++ Team417/src/main/java/org/firstinspires/ftc/team417/MasterTeleOp.java
@@ -1,9 +1,167 @@
 package org.firstinspires.ftc.team417;
 
-/**
- * Created by user_2 on 9/20/2016.
- */
-abstract public class MasterTeleOp extends MasterOpMode
-{
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.Func;
+
+
+/**
+ * Program not used to control Drive-A-Bots.
+ * This can be a good reference for drive controls.
+ */
+@TeleOp(name="Mecanum Drive", group = "Swerve")
+// @Disabled
+public class MasterTeleOp extends MasterOpMode
+{
+    boolean isLiftActivated = false;
+    private ElapsedTime runtime = new ElapsedTime();
+    boolean isSwitchPressed = false;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        // Initialize hardware and other important things
+        initializeRobot();
+
+        // Wait until start button has been pressed
+        waitForStart();
+
+        // Main loop
+        while (opModeIsActive())
+       {
+           isSwitchPressed = digIn.getState();    //  Read the input pin
+            if (gamepad1.b)
+            {
+                if (isLiftActivated == false)
+                {
+                    isLiftActivated = true;
+                    runtime.reset();
+                }
+                else if (runtime.seconds() > 1) // timeout is one second
+                {
+                    isLiftActivated = false;
+                }
+            }
+
+            if (gamepad1.dpad_up && isLiftActivated && !isSwitchPressed) // if the switch is NOT pressed
+            {
+                motorLift.setPower(0.5f);
+            }
+           else
+            {
+                motorLift.setPower(0);
+            }
+            // Gamepads have a new state, so update things that need updating
+            //if(updateGamepads())
+            {
+                mecanumDrive();
+            }
+
+            telemetry.update();
+            idle();
+        }
+    }
+
+    /*
+     * Controls the robot with two joysticks
+     * Left joystick controls the turn / pivot
+     * Right joystick controls the forwards, backwards, left and right
+     */
+
+    public void mecanumDrive()
+    {
+        double frontLeftPower;
+        double frontRightPower;
+        double backLeftPower;
+        double backRightPower;
+
+        double rx;  // represents RIGHT joystick "x axis"
+        double ry;  // represents RIGHT joystick "y axis"
+        double turn; // for turning with LEFT joystick
+        double lx; // represents joystick LEFT "x axis"
+
+        rx = gamepad1.right_stick_x;
+        ry = -gamepad1.right_stick_y; // the joystick is reversed, so make this negative
+        lx = gamepad1.left_stick_x;
+        double jx2; // jx2 and jy2 are the modified variables to the quadratic function
+        double jy2;
+
+        jx2 = modJoyStickInput(rx);
+        jy2 = modJoyStickInput(ry);
+        turn = modJoyStickInput(lx);
+
+        motorFrontLeft.setPower(jx2 + jy2 + turn/2);
+        motorFrontRight.setPower(-jx2 + jy2 - turn/2);
+        motorBackLeft.setPower(-jx2 + jy2 + turn/2);
+        motorBackRight.setPower(jx2 + jy2 - turn/2);
+        // lx is defined as game pad input, then turn gets value from function "modJoyStickInput"
+        // turn used in final equation for each motor
+    }
+
+
+    /* TABLE:
+
+                 FL      FR      BL      BR
+    rotate ->    +        -      +        -
+    rotate <-    -        +      -        +
+    forward      +        +      +        +
+    backward     -        -      -        -
+    left         -        +      +        -
+    right        +        -      -        +
+    d. left      0        +      +        0
+    d. right     +        0      0        +
+
+
+    */
+
+    public double modJoyStickInput(double x) // x is the raw joystick input, refer to "modJoyStickInput"
+    {
+        return Math.pow(x,2) * Math.signum(x);
+    }
+
+
+    public void initializeRobot()
+    {
+        // Initialize motors to be the hardware motors
+        super.initializeHardware(); // comment out this line if blank config
+        // Set up telemetry data
+        configureDashboard();
+    }
+
+    public void configureDashboard()
+    {
+        telemetry.addLine()
+                .addData("Power | FrontLeft: ", new Func<String>() {
+                    @Override public String value() {
+                        return formatNumber(motorFrontLeft.getPower());
+                    }
+                })
+                .addData("Power | FrontRight: ", new Func<String>() {
+                    @Override public String value() {
+                        return formatNumber(motorFrontRight.getPower());
+                    }
+                })
+                .addData("Power | BackLeft: ", new Func<String>() {
+                    @Override public String value() {
+                        return formatNumber(motorBackLeft.getPower());
+                    }
+                })
+                .addData("Power | BackRight: ", new Func<String>() {
+                    @Override public String value() {
+                        return formatNumber(motorBackRight.getPower());
+                    }
+                })
+
+        ;
+
+    }
+
+    public String formatNumber(double d)
+    {
+        return String.format("%.2f", d);
+    }
 }
