f2b6e65cadddb8842fca34d6ec2d42aa9b2382cc
Fromen <drywdude@yahoo.com>
2016-12-02 20:44:38 -0800

    Modified flywheel test to test the new flywheel system.

--- Team8923/src/main/java/org/firstinspires/ftc/team8923/TestFlywheel.java
+++ Team8923/src/main/java/org/firstinspires/ftc/team8923/TestFlywheel.java
@@ -3,94 +3,119 @@ package org.firstinspires.ftc.team8923;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+/*
+ * This OpMode is intended to test the flywheel to check for functionality, and to adjust any
+ * numbers used in the code
+ */
 @TeleOp(name = "Flywheel Test", group = "Tests")
 public class TestFlywheel extends LinearOpMode
 {
-    private DcMotor motorLeft;
-    private DcMotor motorRight;
+    private DcMotor motorFlywheel;
+    private Servo servoFlywheelAngler;
+    private Servo servoBallPopper;
 
-    private double powerLeft = 0.5;
-    private double powerRight = 0.5;
+    private double power = 0.0;
+    private double flywheelAngle = 0.5;
+    private double popperAngle = 0.5;
 
-    private static final double DELTA_POWER = 0.05;
+    private static final double DELTA = 0.05;
 
-    private boolean isRunning = false;
-    private boolean isUsingEncoders = false;
-    private boolean leftWasPressed = false;
-    private boolean rightWasPressed = false;
+    private boolean buttonWasPressed = false;
+
+    // Used to calculate flywheel speed to set appropriate max speed
+    private ElapsedTime timer = new ElapsedTime();
+    private int lastEncoder = 0;
+    private int deltaEncoder = 0;
 
     @Override
     public void runOpMode() throws InterruptedException
     {
-        motorLeft = hardwareMap.dcMotor.get("motorLeft");
-        motorRight = hardwareMap.dcMotor.get("motorRight");
+        motorFlywheel = hardwareMap.dcMotor.get("motorFlywheel");
+        motorFlywheel.setMaxSpeed(2700);
+        motorFlywheel.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorFlywheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        servoFlywheelAngler = hardwareMap.servo.get("servoFlywheelAngler");
+        servoBallPopper = hardwareMap.servo.get("servoBallPopper");
 
         waitForStart();
 
+        telemetry.log().add("Flywheel Angle: D-pad up and down");
+        telemetry.log().add("Popper Angle: D-pad left and right");
+        telemetry.log().add("Flywheel Power: y and a");
+
         while(opModeIsActive())
         {
-            // Toggle run state and encoder state
-            if(gamepad1.start)
-                isRunning = !isRunning;
-            if(gamepad1.back)
-                isUsingEncoders = !isUsingEncoders;
-
-            // Adjust motor powers. Dpad up and down for left motor, and a and y buttons for right
-            if(gamepad1.dpad_up && !leftWasPressed)
+            if(gamepad1.dpad_up)
             {
-                powerLeft += DELTA_POWER;
-                leftWasPressed = true;
+                if(!buttonWasPressed)
+                {
+                    flywheelAngle += DELTA;
+                    buttonWasPressed = true;
+                }
             }
-            else if(gamepad1.dpad_down && !leftWasPressed)
+            else if(gamepad1.dpad_down)
             {
-                powerLeft -= DELTA_POWER;
-                leftWasPressed = true;
+                if(!buttonWasPressed)
+                {
+                    flywheelAngle -= DELTA;
+                    buttonWasPressed = true;
+                }
             }
-            else if(!gamepad1.dpad_down && !gamepad1.dpad_up)
-                leftWasPressed = false;
-
-            if(gamepad1.y && !leftWasPressed)
+            else if(gamepad1.dpad_right)
             {
-                powerRight += DELTA_POWER;
-                rightWasPressed= true;
+                if(!buttonWasPressed)
+                {
+                    popperAngle += DELTA;
+                    buttonWasPressed = true;
+                }
             }
-            else if(gamepad1.a && !leftWasPressed)
+            else if(gamepad1.dpad_left)
             {
-                powerRight -= DELTA_POWER;
-                rightWasPressed = true;
+                if(!buttonWasPressed)
+                {
+                    popperAngle -= DELTA;
+                    buttonWasPressed = true;
+                }
             }
-            else if(!gamepad1.y && !gamepad1.a)
-                rightWasPressed = false;
-
-            // Toggle encoders if necessary
-            if(isUsingEncoders && motorLeft.getMode() != DcMotor.RunMode.RUN_USING_ENCODER)
+            else if(gamepad1.y)
             {
-                motorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                motorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                if(!buttonWasPressed)
+                {
+                    power += DELTA;
+                    buttonWasPressed = true;
+                }
             }
-            else if(!isUsingEncoders && motorLeft.getMode() != DcMotor.RunMode.RUN_WITHOUT_ENCODER)
+            else if(gamepad1.a)
             {
-                motorLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-                motorRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-            }
-
-            // Set power if running or stop
-            if(isRunning)
-            {
-                motorLeft.setPower(powerLeft);
-                motorRight.setPower(powerRight);
+                if(!buttonWasPressed)
+                {
+                    power -= DELTA;
+                    buttonWasPressed = true;
+                }
             }
             else
-            {
-                motorLeft.setPower(0.0);
-                motorRight.setPower(0.0);
-            }
+                buttonWasPressed = false;
 
-            // Inform drivers
-            telemetry.addData("Left", powerLeft);
-            telemetry.addData("Right", powerRight);
-            telemetry.addData("Using Encoders", isUsingEncoders);
+            motorFlywheel.setPower(power);
+            servoFlywheelAngler.setPosition(flywheelAngle);
+            servoBallPopper.setPosition(popperAngle);
+
+            int currentEncoder = motorFlywheel.getCurrentPosition();
+            if(currentEncoder - lastEncoder != 0)
+                deltaEncoder = currentEncoder - lastEncoder;
+
+            telemetry.addData("Power", power);
+            telemetry.addData("Ticks Per Second", deltaEncoder / timer.seconds());
+            telemetry.addData("Angle", flywheelAngle);
+            telemetry.addData("Popper", popperAngle);
+            telemetry.addData("Button Was Pressed", buttonWasPressed);
+
+            timer.reset();
+            lastEncoder = currentEncoder;
 
             telemetry.update();
             idle();
