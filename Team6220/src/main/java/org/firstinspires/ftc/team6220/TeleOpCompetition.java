d06184d24e6b0343e2fc9aa47792ec130ea35f21
Miyu <miyukireus@outlook.com>
2016-12-18 16:42:57 -0800

    spacing fix

--- Team6220/src/main/java/org/firstinspires/ftc/team6220/TeleOpCompetition.java
+++ Team6220/src/main/java/org/firstinspires/ftc/team6220/TeleOpCompetition.java
@@ -44,7 +44,8 @@ public class TeleOpCompetition extends MasterTeleOp
                 collectorMotor.setPower(1.0);
                 //we dont want the 2nd stage servo to reverse and pull out an already troughed particle
                 collectorServo.setPosition(0.5);
-            } else if (driver2.isButtonPressed(Button.B))
+            }
+            else if (driver2.isButtonPressed(Button.B))
             {
                 collectorMotor.setPower(-1.0);
                 collectorServo.setPosition(0.0);
