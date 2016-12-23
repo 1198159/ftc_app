a6206fc9ca8320c379af6792e92b3d1114f876d6
Dryw Wade <drywdude@yahoo.com>
2016-12-22 22:49:14 -0800

    Changed beacon pushing to use one method. Another method was created to drive reliatve to a vision target for this purpose.

--- Team8923/src/main/java/org/firstinspires/ftc/team8923/AutonomousCompetition.java
+++ Team8923/src/main/java/org/firstinspires/ftc/team8923/AutonomousCompetition.java
@@ -11,6 +11,10 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 @Autonomous(name = "Auto Competition", group = "Competition")
 public class AutonomousCompetition extends MasterAutonomous
 {
+    private double beaconX;
+    private double beaconY;
+    private double beaconAngle;
+
     @Override
     public void runOpMode() throws InterruptedException
     {
@@ -102,9 +106,6 @@ public class AutonomousCompetition extends MasterAutonomous
     // method runs the corresponding method, and also sets the coordinates based on selected beacon
     private void pressBeacon(Objectives objective) throws InterruptedException
     {
-        double beaconX;
-        double beaconY;
-
         switch(alliance)
         {
             case RED:
@@ -121,7 +122,7 @@ public class AutonomousCompetition extends MasterAutonomous
                     // Something is a bogus value, so run away
                     default: return;
                 }
-                pressBeaconRed(beaconX, beaconY);
+                beaconAngle = 90.0;
                 break;
             case BLUE:
                 switch(objective)
@@ -137,22 +138,33 @@ public class AutonomousCompetition extends MasterAutonomous
                     // Something is a bogus value, so run away
                     default: return;
                 }
-                pressBeaconBlue(beaconX, beaconY);
+                beaconAngle = 0.0;
                 break;
+            // Something is a bogus value, so run away
+            default: return;
         }
+        pressBeacon();
     }
 
-    // TODO: Find a way to combine the red and blue methods
-    // This is separate from pressBeaconBlue, because combining them is difficult
-    private void pressBeaconRed(double beaconX, double beaconY) throws InterruptedException
+    private void pressBeacon() throws InterruptedException
     {
         // Distance from which we look at the vision target and beacon in mm
-        double observationDistance = 650;
+        double observationDistance = 300;
 
-        turnAndDrive(beaconX, beaconY - observationDistance);
-
-        // Face vision target
-        turnToAngle(90);
+        // Drive in front of the beacon, then face vision target
+        switch(alliance)
+        {
+            case RED:
+                turnAndDrive(beaconX, beaconY - observationDistance);
+                turnToAngle(90.0);
+                break;
+            case BLUE:
+                turnAndDrive(beaconX - observationDistance, beaconY);
+                turnToAngle(0.0);
+                break;
+            // Something is a bogus value, so run away
+            default: return;
+        }
 
         // Give Vuforia a chance to start tracking the target
         sleep(500);
@@ -161,7 +173,7 @@ public class AutonomousCompetition extends MasterAutonomous
         lookForVisionTarget();
 
         // Reposition after tracking target
-        driveToPoint(beaconX, beaconY - observationDistance, 90);
+        driveRelativeToBeacon(0.0, observationDistance);
 
         // Get colors of both sides of beacon. Parameters are in mm from center of vision target
         // Sample location is lowest inside corner of beacon's colored regions
@@ -181,78 +193,8 @@ public class AutonomousCompetition extends MasterAutonomous
         telemetry.log().add("Left hue: " + colorLeft[0]);
         telemetry.log().add("Right hue: " + colorRight[0]);
 
-        /*
-         * Compare the hues of each side. The hue color wheel has red at 360 degrees, and blue at
-         * 240 degrees. Subtracting the angles from each other results in some positive or negative
-         * number. The sign can tell us which side is red and blue. In this case, the left hue is
-         * subtracted from the right hue; a positive sign means left is red, negative mean right.
-         */
-        double buttonDistance;
-        if(colorLeft[0] - colorRight[0] > 0)
-        {
-            telemetry.log().add("Left is red");
-            // Left button is -65 mm from center of beacon
-            buttonDistance = -65;
-        }
-        else
-        {
-            telemetry.log().add("Right is red");
-            // Right button is 65 mm from center of beacon
-            buttonDistance = 65;
-        }
-
-        // Extend pusher to press button
-        servoBeaconPusher.setPosition(ServoPositions.BEACON_EXTEND.pos);
-
-        // Line up with button
-        driveToPoint(beaconX + buttonDistance, beaconY - observationDistance, 90, 0.3);
-        // Move forward to press button
-        driveToPoint(beaconX + buttonDistance, beaconY - 155, 90, 0.3);
-        sleep(500); // TODO: Is this needed?
-        // Back away from beacon
-        driveToPoint(beaconX, beaconY - 250, 90, 0.3);
-
-        // Retract pusher to prevent damage or anything else bad
-        servoBeaconPusher.setPosition(ServoPositions.BEACON_RETRACT.pos);
-    }
-
-    // This is separate from pressBeaconRed, because combining them is difficult
-    private void pressBeaconBlue(double beaconX, double beaconY) throws InterruptedException
-    {
-        // Distance from which we look at the vision target and beacon in mm
-        double observationDistance = 650;
-
-        turnAndDrive(beaconX - observationDistance, beaconY);
-
-        // Face vision target
-        turnToAngle(0);
-
-        // Give Vuforia a chance to start tracking the target
-        sleep(500);
-
-        // Only actually looks if vision target isn't visible
-        lookForVisionTarget();
-
-        // Reposition after tracking target
-        driveToPoint(beaconX - observationDistance, beaconY, 0);
-
-        // Get colors of both sides of beacon. Parameters are in mm from center of vision target
-        // Sample location is lowest inside corner of beacon's colored regions
-        // Float arrays are in HSV format, where 0 index is hue (which we care about)
-        float[] colorLeft = new float[3];
-        float[] colorRight = new float[3];
-        Color.colorToHSV(vuforiaLocator.getPixelColor(-40, 170, 30), colorLeft);
-        Color.colorToHSV(vuforiaLocator.getPixelColor(40, 170, 30), colorRight);
-
-        // Red value will sometimes be near 0 rather than 360. If so, make it above 360
-        // We never get any values near 90 degrees, so anything lower must be red
-        if(colorLeft[0] < 90)
-            colorLeft[0] += 360;
-        if(colorRight[0] < 90)
-            colorRight[0] += 360;
-
-        telemetry.log().add("Left hue: " + colorLeft[0]);
-        telemetry.log().add("Right hue: " + colorRight[0]);
+        // Buttons on beacon are 65mm from center
+        double buttonDistance = 65;
 
         /*
          * Compare the hues of each side. The hue color wheel has red at 360 degrees, and blue at
@@ -260,33 +202,28 @@ public class AutonomousCompetition extends MasterAutonomous
          * number. The sign can tell us which side is red and blue. In this case, the left hue is
          * subtracted from the right hue; a positive sign means left is red, negative mean right.
          */
-        double buttonDistance;
+        // Make button distance negative if we need to go to the other side
         if(colorLeft[0] - colorRight[0] > 0)
-        {
-            telemetry.log().add("Right is blue");
-            // Left button is -65 mm from center of beacon
-            buttonDistance = -65;
-        }
-        else
-        {
-            telemetry.log().add("Left is blue");
-            // Right button is 65 mm from center of beacon
-            buttonDistance = 65;
-        }
+            buttonDistance *= -1;
+        if(alliance == Alliance.BLUE)
+            buttonDistance *= -1;
 
         // Extend pusher to press button
         servoBeaconPusher.setPosition(ServoPositions.BEACON_EXTEND.pos);
+        // Drive slower for more accuracy
+        slowModeDivisor = 2.0;
 
         // Line up with button
-        driveToPoint(beaconX - observationDistance, beaconY + buttonDistance, 0, 0.3);
+        driveRelativeToBeacon(buttonDistance, observationDistance);
         // Move forward to press button
-        driveToPoint(beaconX - 170, beaconY + buttonDistance, 0, 0.3);
-        sleep(500); // TODO: Is this needed?
+        driveRelativeToBeacon(buttonDistance, 155);
         // Back away from beacon
-        driveToPoint(beaconX - 200, beaconY, 0, 0.3);
+        driveRelativeToBeacon(buttonDistance, observationDistance);
 
         // Retract pusher to prevent damage or anything else bad
         servoBeaconPusher.setPosition(ServoPositions.BEACON_RETRACT.pos);
+        // Reset slow mode
+        slowModeDivisor = 1.0;
     }
 
     // TODO: Test me
@@ -328,5 +265,19 @@ public class AutonomousCompetition extends MasterAutonomous
 
         // Stop flywheel
         motorFlywheel.setPower(0.0);
+    }
+
+    // Drives robot with coordinates relative to beacon. Parameters are coordinates intrinsic to
+    // beacon, which are then converted to extrinsic coordinates to which the robot drives
+    private void driveRelativeToBeacon(double targetX, double targetY) throws InterruptedException
+    {
+        // Y input is always positive, but we need it to be negative for the math
+        targetY *= -1;
+
+        // Convert intrinsic values to extrinsic
+        double relativeX = targetX * Math.sin(Math.toRadians(beaconAngle)) + targetY * Math.cos(Math.toRadians(beaconAngle));
+        double relativeY = targetX * -Math.cos(Math.toRadians(beaconAngle)) + targetY * Math.sin(Math.toRadians(beaconAngle));
+
+        driveToPoint(beaconX + relativeX, beaconY + relativeY, beaconAngle);
     }
 }
