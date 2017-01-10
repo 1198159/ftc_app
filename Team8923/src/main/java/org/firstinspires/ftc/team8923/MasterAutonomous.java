684dc5b8027d7c3f56e21a0c55c576af9382aaa6
Coleson <thenorthernquasar@gmail.com>
2017-01-09 20:57:14 -0800

    Made the autonomous drive faster to complete requested tasks within time.

--- Team8923/src/main/java/org/firstinspires/ftc/team8923/MasterAutonomous.java
+++ Team8923/src/main/java/org/firstinspires/ftc/team8923/MasterAutonomous.java
@@ -58,10 +58,10 @@ abstract class MasterAutonomous extends Master
 
     // Constants for robot in autonomous
     // Max drive power is less than 1 to ensure speed controller works
-    private static final double MAX_DRIVE_POWER = 0.8;
+    private static final double MAX_DRIVE_POWER = 1.0;
     private static final double MIN_DRIVE_POWER = 0.15;
     private static final double TURN_POWER_CONSTANT = 1.0 / 150.0;
-    private static final double DRIVE_POWER_CONSTANT = 1.0 / 1000.0;
+    private static final double DRIVE_POWER_CONSTANT = 1.0 / 650.0;
 
     enum Objectives
     {
