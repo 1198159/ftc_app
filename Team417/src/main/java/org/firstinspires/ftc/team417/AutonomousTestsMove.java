21238aa1a32b017767e670800000ed7f1c16d77a
Madeline <maddynnguyen@gmail.com>
2017-01-22 15:50:06 -0800

    Created a new class for move tests

---
+++ Team417/src/main/java/org/firstinspires/ftc/team417/AutonomousTestsMove.java
@@ -0,0 +1,173 @@
+package org.firstinspires.ftc.team417;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="Autonomous Tests 2", group = "Swerve")
+// @Disabled
+
+public class AutonomousTestsMove extends MasterAutonomous
+{
+
+    public void runOpMode() throws InterruptedException
+    {
+        // Initialize hardware and other important things
+        initializeRobot();
+
+        VuforiaNav.initVuforia();
+        telemetry.addData("Path", "Select Team and Pos...");
+
+        // Wait until we're told to go
+        while (!isStarted())
+        {
+            // allow driver to choose a team
+            if (gamepad1.b)
+            {
+                isRedTeam = true;
+            }
+
+            if (gamepad1.x)
+            {
+                isRedTeam = false;
+            }
+
+            // select position one or two, one is closer to the origin
+            if (gamepad1.y)
+            {
+                isStartingPosOne = true;
+            }
+            if (gamepad1.a)
+            {
+                isStartingPosOne = false;
+            }
+
+            if (isRedTeam)
+            {
+                if (isStartingPosOne)
+                {
+                    telemetry.addData("Team: ", "Red 1");
+                }
+                else
+                {
+                    telemetry.addData("Team: ", "Red 2");
+                }
+            }
+            else
+            {
+                if (isStartingPosOne)
+                {
+                    telemetry.addData("Team: ", "Blue 1");
+                }
+                else
+                {
+                    telemetry.addData("Team: ", "Blue 2");
+                }
+            }
+            telemetry.update();
+            idle();
+        }
+        telemetry.update();
+
+        if (isRedTeam) // if team RED
+        {
+            if (isStartingPosOne)
+            {
+                // OPTION RED ONE (TOOLS)
+                startDelay = 2000;
+                pivotAngle = 55; // pivot this amount before acquiring target
+                targetAngle = 0; // Vuforia angle
+                startDist = 2286;
+                targetIndex = 1;
+                targetPos[0] = 2743.2f;
+                targetPos[1] = mmFTCFieldWidth;
+                //telemetry.addData("Team: ", "Red 1"); // display what team we're on after choosing with the buttons
+            }
+            else
+            {
+                // OPTION RED TWO (GEARS)
+                startDelay = 0;
+                pivotAngle = 55; // pivot this amount before acquiring target
+                targetAngle = 0; // Vuforia angle
+                startDist = 1270;
+                targetIndex = 3;
+                targetPos[0] = 1524;
+                targetPos[1] = mmFTCFieldWidth;
+                //telemetry.addData("Team: ", "Red 2"); // display what team we're on after choosing with the buttons
+            }
+            telemetry.update();
+        }
+        else // if team BLUE
+        {
+            if (isStartingPosOne)
+            {
+                // OPTION BLUE ONE (LEGOS)
+                startDelay = 2000;
+                pivotAngle = -55; // recalc pivot?? also for red one??
+                targetAngle = -90;
+                startDist = 2286;
+                targetIndex = 2;
+                targetPos[0] = mmFTCFieldWidth;
+                targetPos[1] = 2743.2f;
+                //telemetry.addData("Team: ", "Blue 1");
+            }
+            else
+            {
+                // OPTION BLUE TWO (WHEELS)
+                startDelay = 0;
+                pivotAngle = -55;
+                targetAngle = -90;
+                startDist = 1270;
+                targetIndex = 0;
+                targetPos[0] = mmFTCFieldWidth;
+                targetPos[1] = 1524;
+                //telemetry.addData("Team: ", "Blue 2");
+            }
+        }
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        VuforiaNav.startTracking();
+        //     pause(startDelay);
+        VuforiaNav.getLocation(targetIndex);
+
+        //pause(delay);
+
+        /*
+        while (opModeIsActive()) {
+
+            VuforiaNav.getLocation();
+            telemetry.addData("tracking ", VuforiaNav.isVisible() ? "Visible" : "Not Visible");
+            if (VuforiaNav.lastLocation != null)
+            {
+                telemetry.addData("location ", format(VuforiaNav.lastLocation));
+            }
+
+            telemetry.update();
+            //pause(500);
+        }
+        */
+
+
+        // shoot twice
+        motorLauncher.setPower(0.7);
+        pause(1500);
+        servoParticle.setPosition(0.8);
+        pause(400);
+        servoParticle.setPosition(0.0);
+        pause(1200);
+        servoParticle.setPosition(0.8);
+        pause(400);
+        servoParticle.setPosition(0.0);
+        motorLauncher.setPower(0.0);
+
+        pause(20000);
+
+        move(0, -2149, 0.7, 4);
+        pause(500);
+        move(0, -100, 0.4, 3);
+
+        telemetry.addData("Path", "Complete");
+        telemetry.update();
+    }
+
+}
