1aa8e58396fa94bc572cd863af9c47be21f1b19a
Madeline <maddynnguyen@gmail.com>
2017-04-11 20:55:20 -0700

    Tightened low position of left pusher

--- Team417/src/main/java/org/firstinspires/ftc/team417/MasterOpMode.java
+++ Team417/src/main/java/org/firstinspires/ftc/team417/MasterOpMode.java
@@ -57,7 +57,7 @@ abstract public class MasterOpMode extends LinearOpMode
 
     static final double LEFT_PUSHER_HIGH = 0.77;
     static final double RIGHT_PUSHER_HIGH = 0.88;
-    static final double LEFT_PUSHER_LOW = 0.01;
+    static final double LEFT_PUSHER_LOW = 0.00;
     static final double RIGHT_PUSHER_LOW = 0.12;
 
 
