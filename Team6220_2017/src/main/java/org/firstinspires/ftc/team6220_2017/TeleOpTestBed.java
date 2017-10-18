b4d1e9eb9a519406a6ef3cac27657deeec25ae8a
Cole Welch <colewelch151@gmail.com>
2017-10-17 20:56:15 -0700

    Finished getJewelColor and added test for it to teleopTestBed.  Currently doesn't work, but doesn't break code.

--- Team6220_2017/src/main/java/org/firstinspires/ftc/team6220_2017/TeleOpTestBed.java
+++ Team6220_2017/src/main/java/org/firstinspires/ftc/team6220_2017/TeleOpTestBed.java
@@ -17,21 +17,34 @@ public class TeleOpTestBed extends MasterOpMode
     CRServo rightServo;
     CRServo leftServo;
     Servo servo;
+    VuforiaHelper vuforiaHelper = new VuforiaHelper();
 
     @Override public void runOpMode() throws InterruptedException
     {
         driver1 = new DriverInput(gamepad1);
         driver2 = new DriverInput(gamepad2);
 
+        //telemetry.log().setCapacity(4);
         /*rightServo = hardwareMap.crservo.get("servoRight");
         leftServo = hardwareMap.crservo.get("servoLeft");*/
-        servo = hardwareMap.servo.get("servo");
+        //servo = hardwareMap.servo.get("servo");
+
+        vuforiaHelper.setupVuforia();
         // Wait until start button has been pressed
         waitForStart();
 
         // Main loop
         while(opModeIsActive())
         {
+            if(gamepad1.x)
+            {
+                vuforiaHelper.getJewelColor();
+                //output red hue values of both sides for debugging
+                telemetry.addData("LeftSideHue: ", vuforiaHelper.leftColorOutput[0]);
+                telemetry.addData("RightSideHue: ", vuforiaHelper.rightColorOutput[0]);
+                telemetry.update();
+            }
+
             //for testing glyph collection; servos on either side of glyph pull or push it
             if(gamepad1.a)
             {
@@ -45,7 +58,7 @@ public class TeleOpTestBed extends MasterOpMode
             }
 
             //for robot arm test
-            servo.setPosition((gamepad1.left_stick_y + 1) / 2);
+            //servo.setPosition((gamepad1.left_stick_y + 1) / 2);
 
             idle();
         }
