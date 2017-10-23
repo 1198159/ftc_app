acf3be746b6d85cde0b96dae1a78811ca7d3b644
Madeline Nguyen <maddynnguyen@gmail.com>
2017-10-22 23:08:29 -0700

    Created autonomous tests for encoder and dead reckoning

---
+++ Team417_2017/src/main/java/org/firstinspires/ftc/team417_2017/AutonomousMoveTests.java
@@ -0,0 +1,92 @@
+package org.firstinspires.ftc.team417_2017;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name="Autonomous Move Tests", group = "Swerve")
+// @Disabled
+
+public class AutonomousMoveTests extends MasterAutonomous
+{
+
+    public void runOpMode() throws InterruptedException
+    {
+        // Initialize hardware and other important things
+        super.initializeHardware();
+
+        telemetry.addData("Path", "Select Team and Pos...");
+
+        // Wait until we're told to go
+        while (!isStarted())
+        {
+            // allow driver to choose a team
+            if (gamepad1.b) isRedTeam = true;
+            if (gamepad1.x) isRedTeam = false;
+
+            // select position one or two, one is closer to the origin
+            if (gamepad1.y) isStartingPosOne = true;
+            if (gamepad1.a) isStartingPosOne = false;
+
+            if (isRedTeam)
+            {
+                if (isStartingPosOne) telemetry.addData("Team: ", "Red 1");
+                else telemetry.addData("Team: ", "Red 2");
+            }
+            else
+            {
+                if (isStartingPosOne) telemetry.addData("Team: ", "Blue 1");
+                else telemetry.addData("Team: ", "Blue 2");
+            }
+            telemetry.update();
+            idle();
+        }
+        telemetry.update();
+
+        if (isRedTeam) // if team RED
+        {
+            if (isStartingPosOne)
+            {
+
+            }
+            else
+            {
+
+            }
+            telemetry.update();
+        }
+        else // if team BLUE
+        {
+            if (isStartingPosOne)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+
+// Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        autoRuntime.reset(); // set the 30 second timer
+
+// START OF AUTONOMOUS
+
+        TOL_ANGLE = 1.0;
+        TOL = 60;
+        Kmove = 1.0/1200.0;
+        Kpivot = 1/140.0;
+        MINSPEED = 0.1;
+        PIVOT_MINSPEED = 0.1;
+
+        double refAngle = imu.getAngularOrientation().firstAngle;
+        //moveMaintainHeading(0, 100, 0, refAngle, 0.5, 3);
+        //move(0, 300, 0.4, 3);
+        //moveAverage(0, 100, 0, 0.5, 3); // still doesn't work
+        //pivot(10, 0.5);
+        pivotWithReference(10, refAngle, 0.1);
+
+
+        telemetry.addData("Autonomous", "Complete");
+        telemetry.update();
+    }
+}
