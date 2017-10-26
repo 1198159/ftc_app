12723fdcabcbcebebf6a35d3cfc4878db16fe415
Madeline Nguyen <maddynnguyen@gmail.com>
2017-10-25 23:13:37 -0700

    Put in software to switch encoder modes during TeleOp

--- Team417_2017/src/main/java/org/firstinspires/ftc/team417_2017/TeleOpCompetition.java
+++ Team417_2017/src/main/java/org/firstinspires/ftc/team417_2017/TeleOpCompetition.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.team417_2017;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp(name = "TeleOp Competition")
 public class TeleOpCompetition extends MasterTeleOp
@@ -14,7 +15,30 @@ public class TeleOpCompetition extends MasterTeleOp
         waitForStart();
         while (opModeIsActive())
         {
+            if (gamepad1.a)
+            {
+                motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                motorBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                motorBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            }
+            if (gamepad1.b)
+            {
+                motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                motorFL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                motorFR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                motorBL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+                motorBR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+            }
             omniDriveTeleOp();
+            // TODO: add some telemetry to display the motor power
             idle();
         }
     }
