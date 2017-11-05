7b5f172759cc6db0d49370831e1f36b83b0e4a1b
Steve Geffner <steve.geffner.phd@gmail.com>
2017-11-04 20:33:09 -0700

    added CR Servo test opmode

--- TeamSwerve/src/main/java/org/firstinspires/ftc/teamswerve/CRServoTest.java
+++ TeamSwerve/src/main/java/org/firstinspires/ftc/teamswerve/CRServoTest.java
@@ -1,38 +1,32 @@
 package org.firstinspires.ftc.teamswerve;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.LED;
 import com.qualcomm.robotcore.hardware.Servo;
-
-import org.firstinspires.ftc.robotcore.external.Func;
 
 
 /**
  * Program used to test a servo
  */
-@TeleOp(name="ServoTest", group = "Swerve")
+@TeleOp(name="CRServoTest", group = "Swerve")
 //@Disabled
-public class ServoTest extends LinearOpMode {
-    Servo servo = null;
+public class CRServoTest extends LinearOpMode {
+    CRServo servo = null;
 
     @Override
     public void runOpMode() throws InterruptedException {
         // Initialize hardware and other important things
-        servo = hardwareMap.servo.get("servo");
+        servo = hardwareMap.crservo.get("servo");
 
         // Wait until start button has been pressed
         waitForStart();
 
         // Main loop
         while (opModeIsActive()) {
-            servo.setPosition(0);
+            servo.setPower(0);
             pause(1000);
-            servo.setPosition(1);
+            servo.setPower(1);
             pause(1000);
             idle();
         }
