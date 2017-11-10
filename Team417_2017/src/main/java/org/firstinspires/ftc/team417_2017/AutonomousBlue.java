8ddb79b1455403d799d0b076cec7c5f639626961
Madeline Nguyen <maddynnguyen@gmail.com>
2017-11-09 19:07:45 -0800

    Worked on Autonomous Option Blue, still debugging

--- Team417_2017/src/main/java/org/firstinspires/ftc/team417_2017/AutonomousBlue.java
+++ Team417_2017/src/main/java/org/firstinspires/ftc/team417_2017/AutonomousBlue.java
@@ -50,12 +50,6 @@ public class AutonomousBlue extends MasterAutonomous
 
 // START OF AUTONOMOUS
 
-        // lower the servos, putting jewel manipulator into position
-        servoJewelStore.setPosition(JEWEL_STORE_LOW);
-        sleep(200);
-        servoJewelDrop.setPosition(JEWEL_DROP_LOW);
-        sleep(200);
-
         Kmove = 1.0/1200.0;
         MINSPEED = 0.3;
         TOL_ANGLE = 3.0;
@@ -63,10 +57,12 @@ public class AutonomousBlue extends MasterAutonomous
         Kpivot = 1/150.0;
         PIVOT_MINSPEED = 0.2;
 
+        // set the reference angle
+        double refAngle = imu.getAngularOrientation().firstAngle;
+
         if (VuforiaDetect.isVisible()) // if vuMark seen is not unknown,
         {
             VuforiaDetect.GetVumark();
-            VuforiaDetect.GetLeftJewelColor();
                 /* Found an instance of the template. In the actual game, you will probably
                  * loop until this condition occurs, then move on to act accordingly depending
                  * on which VuMark was visible. */
@@ -78,42 +74,42 @@ public class AutonomousBlue extends MasterAutonomous
                 telemetry.addData("leftHue ", VuforiaDetect.avgLeftJewelColor);
                 telemetry.addData("rightHue ", VuforiaDetect.avgRightJewelColor);
                 telemetry.addData("isLeftJewelBlue", VuforiaDetect.isLeftJewelBlue);
+                telemetry.update();
+
+                if(VuforiaDetect.isLeftJewelBlue) // if the left jewel is blue,
+                {
+                    pivotWithReference(-30, refAngle, 0.5); // then pivot right
+                    sleep(200);
+                    servoJewelStore.setPosition(JEWEL_STORE_INIT);
+                    sleep(200);
+                    servoJewelDrop.setPosition(JEWEL_DROP_INIT);
+                    sleep(200);
+                    pivotWithReference(0, refAngle, 0.5); // then pivot back
+                    sleep(200);
+                }
+                else // if the left jewel is red,
+                {
+                    pivotWithReference(30, refAngle, 0.5); // then pivot left
+                    sleep(200);
+                    servoJewelStore.setPosition(JEWEL_STORE_INIT);
+                    sleep(200);
+                    servoJewelDrop.setPosition(JEWEL_DROP_INIT);
+                    sleep(200);
+                    pivotWithReference(0, refAngle, 0.5); // then pivot back
+                    sleep(200);
+                }
             }
         }
         else
         {
             telemetry.addData("VuMark", "not visible");
+            // TODO: write software for guessing
         }
         telemetry.update();
 
-        sleep(200);
-
-        // set the reference angle
-        double refAngle = imu.getAngularOrientation().firstAngle;
-
-        if(VuforiaDetect.isLeftJewelBlue) // if the left jewel is blue,
-        {
-            pivotWithReference(-30, refAngle, 0.5); // then pivot right
-            sleep(200);
-            servoJewelStore.setPosition(JEWEL_STORE_INIT);
-            sleep(200);
-            servoJewelDrop.setPosition(JEWEL_DROP_INIT);
-            sleep(200);
-            pivotWithReference(0, refAngle, 0.5); // then pivot back
-            sleep(200);
-        }
-        else // if the left jewel is red,
-        {
-            pivotWithReference(30, refAngle, 0.5); // then pivot left
-            sleep(200);
-            servoJewelStore.setPosition(JEWEL_STORE_INIT);
-            sleep(200);
-            servoJewelDrop.setPosition(JEWEL_DROP_INIT);
-            sleep(200);
-            pivotWithReference(0, refAngle, 0.5); // then pivot back
-            sleep(200);
-        }
-
+        // move servos back
+        servoJewelStore.setPosition(JEWEL_STORE_INIT);
+        servoJewelDrop.setPosition(JEWEL_DROP_INIT);
 
         if (isPosLeft) // BLUE LEFT
         {
