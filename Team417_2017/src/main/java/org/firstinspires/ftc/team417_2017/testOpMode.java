999de20586e3129c875543703bb2da9813b42577
Madeline Nguyen <maddynnguyen@gmail.com>
2018-01-14 23:20:46 -0800

    Tested run using encoder mode

--- Team417_2017/src/main/java/org/firstinspires/ftc/team417_2017/testOpMode.java
+++ Team417_2017/src/main/java/org/firstinspires/ftc/team417_2017/testOpMode.java
@@ -18,30 +18,30 @@ public class testOpMode extends LinearOpMode
 
     @Override public void runOpMode() throws InterruptedException
     {
-        // Wait until start button has been pressed
-        waitForStart();
-
         motor = hardwareMap.dcMotor.get("motor");
         //motor.setDirection(DcMotor.Direction.REVERSE); // reverse the motor
         motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT); // allows the motor to slow down without brakes
         //motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER); // this mode simply inputs power, so no PID
-        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER); // this mode will do its best to run at a targeted velocity
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION); // this mode will do its best to run at a targeted velocity
 
         // Tell the user that the motor is done initializing
         telemetry.addData(">", "Motor done initializing");
         telemetry.update();
-        motor.setPower(-0.5);
-        sleep(30000);
-        motor.setPower(0.0);
-        idle();
 
-        /*
+        // Wait until start button has been pressed
+        waitForStart();
+        int tarPos = motor.getCurrentPosition() - 1120;
+        motor.setTargetPosition(tarPos);
+        motor.setPower(0.7);
         // Main loop
         while(opModeIsActive())
         {
-
+            if (!motor.isBusy())
+               motor.setPower(0.0);
+            idle();
         }
-        */
+
     }
 
 }
