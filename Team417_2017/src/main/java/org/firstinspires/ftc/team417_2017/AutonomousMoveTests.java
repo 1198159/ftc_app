181311cdb410ffd84ec0bf0a1f9990b0c5939318
Madeline Nguyen <maddynnguyen@gmail.com>
2018-02-04 15:14:48 -0800

    Tested knocking the jewel off in a softer manner

--- Team417_2017/src/main/java/org/firstinspires/ftc/team417_2017/AutonomousMoveTests.java
+++ Team417_2017/src/main/java/org/firstinspires/ftc/team417_2017/AutonomousMoveTests.java
@@ -23,14 +23,22 @@ public class AutonomousMoveTests extends MasterAutonomous
 
 // START OF AUTONOMOUS
 
-        closeGG(); // -500
-        motorGlyphGrab.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        openGG(-400); // open the GG a little bit
+        double refAngle = imu.getAngularOrientation().firstAngle; // possibly move to initialization
+        Kmove = 1.0/1200.0;
+        TOL = 100.0;
+        TOL_ANGLE = 2;
+        Kpivot = 1/200.0;
 
+        // lower the servos, putting jewel manipulator into position
+        servoJewel.setPosition(JEWEL_LOW);
         sleep(200);
-        // release the glyph
-        motorGlyphGrab.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        openGG(-160);
+
+        pivotWithReference(17, refAngle, 0.12, 0.3); // then pivot left
+        sleep(200);
+        servoJewel.setPosition(JEWEL_INIT);
+        sleep(200);
+        pivotWithReference(0, refAngle, 0.15, 0.3); // then pivot back
+        sleep(200);
 
         telemetry.addData("Autonomous", "Complete");
         telemetry.update();
