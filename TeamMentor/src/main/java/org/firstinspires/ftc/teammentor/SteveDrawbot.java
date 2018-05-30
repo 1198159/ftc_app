db6b14895994c3e493524488ca673ab1355866bc
Steve Geffner <steve.geffner.phd@gmail.com>
2018-05-29 20:16:00 -0700

    minor modifications to SteveDrawbot, no real functionality yet

--- TeamMentor/src/main/java/org/firstinspires/ftc/teammentor/SteveDrawbot.java
+++ TeamMentor/src/main/java/org/firstinspires/ftc/teammentor/SteveDrawbot.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teammentor;
 
+import android.graphics.Point;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -20,19 +22,21 @@ public class SteveDrawbot extends LinearOpMode
 
     Our drawing bot has the following hardware construction:
 
-    servo 1 (s1) is connected to the base and rotates an arm of length a.
-    servo 2 (s2) is at the end of that arm, and rotates another arm of length b.
+    servo 1 (s1) is connected to a base and rotates an arm of length a ("side a").
+    servo 2 (s2) is at the end of that arm, and rotates another arm of length b ("side b").
     * is the pen
 
-                    <- a ->
-              s2 ------------------ s1
-              |
-          b   |
-              |
-              *
 
-         [hmmm, we need a way to lift and lower the pen...]
-     Let alpha be the angle of servo s1 and let beta be the angle of servo s2.
+                                  *
+                                  |
+                                  | side b
+                      side a      |
+          servo1 ------------------ servo2
+
+     [hmmm, we need a way to lift and lower the pen...]
+
+     Let alpha be the angle of servo1 relative to a baseline
+     and let beta be the angle of servo2 relative to side a (the bar between servo1 and servo2).
 
      We can calculate the required angles using trig. (more about that below)
 
@@ -41,7 +45,7 @@ public class SteveDrawbot extends LinearOpMode
      and have the drawbot ink those locations on a destination piece of paper.
 
      To "draw" the image, we need to convert the desired coordinates for the pen into
-     the two angles (i.e., servo positions for s1 and s2).
+     the two angles (i.e., servo positions for servo1 and servo2).
 
      There is a handy article here that explains the math.
      https://math.stackexchange.com/questions/1423467/calculating-angles-neccessary-to-reach-a-position-on-a-2d-plane-for-two-robot-ar
@@ -52,12 +56,18 @@ public class SteveDrawbot extends LinearOpMode
 
     Servo servo1, servo2;
 
-    Double len_a = 10.0; //length of bar from s1 to s2, in inches
-    Double len_b = 5.0;  //length of bar from s2 to pen, in inches
-    double len_c = 0.0;  //length from s1 to pen, in inches. This will be calculated as needed for each point of the drawing.
+    Double len_a = 10.0; //length of bar from servo1 to servo2
+    Double len_b = 5.0;  //length of bar from servo2 to pen
+    double len_c = 0.0;  //length from servo1 to pen. This will be calculated as needed for each point of the drawing.
 
-    Double angle_alpha = 0.0; //angle for servo s1
-    Double angle_beta = 0.0;  //angle for servo s2
+    //     Let alpha be the angle of servo servo1 relative to a baseline
+    //     and let beta be the angle of servo servo2 relative to side a (the bar between servo1 and servo2).
+    public double angleAlpha;
+    public double angleBeta;
+
+    double DEFAULT_ANGLE_ALPHA = 0.0;
+    double DEFAULT_ANGLE_BETA = 0.0;
+
 
 
     @Override public void runOpMode() throws InterruptedException
@@ -79,15 +89,35 @@ public class SteveDrawbot extends LinearOpMode
         }
     }
 
-
-    public void initializeRobot()
+    private void initializeServoAngles()
     {
+        angleAlpha = DEFAULT_ANGLE_ALPHA;
+        angleBeta = DEFAULT_ANGLE_BETA;
+
+        updateServoPositions();
+    }
+
+
+    private void initializeRobot()
+    {
+        servo1 = hardwareMap.servo.get("servo1");
+        servo2 = hardwareMap.servo.get("servo2");
+
+        initializeServoAngles();
 
         // Set up telemetry data
         //configureDashboard();
     }
+    
 
-    public void configureDashboard()
+    private void updateServoPositions()
+    {
+        servo1.setPosition(angleAlpha);
+        servo2.setPosition(angleBeta);
+    }
+
+
+    private void configureDashboard()
     {
         /*
         telemetry.addLine()
@@ -114,11 +144,11 @@ public class SteveDrawbot extends LinearOpMode
                 */
     }
 
-    public String formatNumber(double d)
+    private String formatNumber(double d)
     {
         return String.format("%.2f", d);
     }
-    public String formatNumberEightDigits(double d)
+    private String formatNumberEightDigits(double d)
     {
         return String.format("%.8f", d);
     }
