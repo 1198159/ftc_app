323b35838450d9e22b2ad78394aac25f3bc2ecea
Cole Welch <colewelch151@gmail.com>
2018-06-27 14:06:40 -0700

    Created program to test read times of various combinations of imu and encoder data.

--- TeamSwerve/src/main/java/ControlHubEncoderIMUPerformanceTest.java
+++ TeamSwerve/src/main/java/ControlHubEncoderIMUPerformanceTest.java
@@ -1,3 +1,4 @@
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -9,7 +10,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 // Many samples are taken over a large number of loops to get more meaningful data.  Retrieval
 // times of encoder values and the z-axis orientation of the imu are measured separately and together.
 
-@Autonomous(name = "EncoderIMUPerfTest", group = "Autonomous")
+@Autonomous(name = "Encoder/IMU Perf Test", group = "Autonomous")
 public class ControlHubEncoderIMUPerformanceTest extends LinearOpMode
 {
     @Override
@@ -17,10 +18,14 @@ public class ControlHubEncoderIMUPerformanceTest extends LinearOpMode
     {
         final int NUM_LOOPS = 1001;
         int loops = 0;
-        double curEncoderValue;
+        double curEncoderValue1 = 0;
+        double curEncoderValue2 = 0;
+        double curEncoderValue3 = 0;
+        double curEncoderValue4 = 0;
         double encoderSum = 0;
-        double[] loopTimesMS = new double[loops];
+        double[] loopTimesMS = new double[NUM_LOOPS];
         ElapsedTime loopTime = new ElapsedTime();
+        double heading = 0;
 
 
         // Set up motors in configuration; some motors may or may not be used for measurements
@@ -38,6 +43,20 @@ public class ControlHubEncoderIMUPerformanceTest extends LinearOpMode
         motorFrontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
 
+        // Retrieve and initialize the IMU. We expect the IMU to be attached to an I2C port
+        // on a Core Device Interface Module, configured to be a sensor of type "AdaFruit IMU",
+        // and named "imu". Certain parameters must be specified before using the imu.
+        BNO055IMU imu;
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "AdafruitIMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+
         waitForStart();
         loopTime.reset();
 
@@ -47,7 +66,14 @@ public class ControlHubEncoderIMUPerformanceTest extends LinearOpMode
             // Go through a preset number of loops while creating an array of the loop times
             while (loops < NUM_LOOPS)
             {
-                curEncoderValue = motorBackLeft.getCurrentPosition();
+                curEncoderValue1 = motorBackLeft.getCurrentPosition();
+                curEncoderValue2 = motorBackRight.getCurrentPosition();
+                curEncoderValue3 = motorFrontLeft.getCurrentPosition();
+                curEncoderValue4 = motorFrontRight.getCurrentPosition();
+
+                //heading = imu.getAngularOrientation().firstAngle;
+                //telemetry.addData("Heading: ", heading);
+                //telemetry.update();
 
                 loopTimesMS[loops] = loopTime.milliseconds();    // Get time elapsed for each loop
                 loopTime.reset();   // Reset timer so we only get time elapsed for individual loops
@@ -65,14 +91,25 @@ public class ControlHubEncoderIMUPerformanceTest extends LinearOpMode
             java.util.Arrays.sort(loopTimesMS);
             double averageTime = encoderSum / NUM_LOOPS;
             double medianTime = loopTimesMS[(NUM_LOOPS - 1)/ 2];
+            double std = 0;
             double maxTime = loopTimesMS[NUM_LOOPS - 1];
+            double minTime = loopTimesMS[0];
+
+            for (int i = 0; i < NUM_LOOPS; i++)
+            {
+                std += Math.pow(loopTimesMS[i] - averageTime, 2);
+            }
+
+            std = Math.sqrt(std / (NUM_LOOPS - 1));
 
             // Display statistics
             while (opModeIsActive())
             {
-                telemetry.addData("med: ", medianTime);
-                telemetry.addData("max: ", maxTime);
                 telemetry.addData("avg: ", averageTime);
+                telemetry.addData("med: ", medianTime);
+                telemetry.addData("std: ", std);
+                telemetry.addData("min: ", minTime);
+                telemetry.addData("max: ", maxTime);
                 telemetry.update();
                 idle();
             }
