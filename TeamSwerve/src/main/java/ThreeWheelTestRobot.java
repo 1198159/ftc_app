1ff8ffdd06b399b883766de8527db62efefe82a6
Cole Welch <colewelch151@gmail.com>
2018-07-05 16:45:34 -0700

    Added angular velocity logging with 9 point triangular filter to 3 wheel robot test.

--- TeamSwerve/src/main/java/ThreeWheelTestRobot.java
+++ TeamSwerve/src/main/java/ThreeWheelTestRobot.java
@@ -16,10 +16,16 @@ public class ThreeWheelTestRobot extends LinearOpMode
     @Override
     public void runOpMode() throws InterruptedException
     {
-        final int NUM_LOOPS = 250;
+        final int NUM_LOOPS = 200;
         ElapsedTime loopTime = new ElapsedTime();
+        // For plotting distance vs. time
         double[] encValues = new double[NUM_LOOPS];
-        double sampleTime = 8;     // This number is in milliseconds
+        // For plotting angular velocity vs. time
+        double[] encoderSpeeds = new double[NUM_LOOPS];
+        double[] encoderFilteredSpeeds = new double[NUM_LOOPS];
+        double sampleTime = 10;     // This number is in milliseconds
+        double newEncVal = 0;
+        double oldEncVal = 0;
 
         // Initialize drive motor, which powers the front wheel
         DcMotor motor;
@@ -36,12 +42,48 @@ public class ThreeWheelTestRobot extends LinearOpMode
         // Go through the specified number of loops while sampling distance traveled from encoders
         for (int i = 0; i < NUM_LOOPS; i++)
         {
+                //** Use this code block for angular velocity vs. time
+            loopTime.reset();
+
+            oldEncVal = newEncVal;
+            newEncVal = motor.getCurrentPosition();
+
+            // Calculate time rate of change of encoder values; sampleTime is converted to seconds
+            encoderSpeeds[i] = (-newEncVal + oldEncVal) / (0.001 * sampleTime);
+            //encoderSpeeds[i] = ((-newEncValBL + newEncValBR - newEncValFL + newEncValFR) / 4
+            //        -(-oldEncValBL + oldEncValBR - oldEncValFL + oldEncValFR) / 4) / (0.001 * sampleTime);
+
+            // Filter data using a triangular filter.  We must modify our calculations for the first
+            // 2 values of i due to our method of filtering
+            if (i == 6)
+            {
+                encoderFilteredSpeeds[0] = encoderSpeeds[0];
+                encoderFilteredSpeeds[1] = (encoderSpeeds[0] + 2 * encoderSpeeds[1] + encoderSpeeds[2]) / 4;
+                encoderFilteredSpeeds[2] = (encoderSpeeds[0] + 2 * encoderSpeeds[1] + 3 * encoderSpeeds[2] +
+                        2 * encoderSpeeds[3] + encoderSpeeds[4]) / 9;
+                encoderFilteredSpeeds[3] = (encoderSpeeds[0] + 2 * encoderSpeeds[1] + 3 * encoderSpeeds[2] +
+                        4 * encoderSpeeds[3] + 3 * encoderSpeeds[4] + 2 * encoderSpeeds[5] + encoderSpeeds[6]) / 16;
+            }
+            else if (i >= 8)
+            {
+                encoderFilteredSpeeds[i - 4] = (encoderSpeeds[i - 8] + 2 * encoderSpeeds[i - 7] +
+                        3 * encoderSpeeds[i - 6] + 4 * encoderSpeeds[i - 5] + 5 * encoderSpeeds[i - 4] +
+                        4 * encoderSpeeds[i - 3] + 3 * encoderSpeeds[i - 2] + 2 * encoderSpeeds[i - 1] +
+                        encoderSpeeds[i]);
+            }
+
+            while (loopTime.milliseconds() < sampleTime && opModeIsActive())
+                idle();
+
+                //** Use this code block for distance vs. time
+            /*
             loopTime.reset();
 
             encValues[i] = -motor.getCurrentPosition();
 
             while (loopTime.milliseconds() < sampleTime)
                 idle();
+            */
         }
 
         motor.setPower(0.0);
