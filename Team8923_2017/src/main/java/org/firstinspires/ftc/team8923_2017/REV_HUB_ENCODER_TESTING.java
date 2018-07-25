85a4f7ea2ed052ea0dfc96c67efbc6caad32cc45
Coleson Oliver <thenorthernquasar@gmail.com>
2018-07-24 19:47:36 -0700

    added code to previous class

--- Team8923_2017/src/main/java/org/firstinspires/ftc/team8923_2017/REV_HUB_ENCODER_TESTING.java
+++ Team8923_2017/src/main/java/org/firstinspires/ftc/team8923_2017/REV_HUB_ENCODER_TESTING.java
@@ -1,5 +1,43 @@
 package org.firstinspires.ftc.team8923_2017;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name = "Rev Hub Encoder Test Program")
 public class REV_HUB_ENCODER_TESTING extends Master
 {
+    DcMotor motor;
+
+    double encoderTicks = 0;
+
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+        ElapsedTime timer = new ElapsedTime();
+        ElapsedTime sleepTimer = new ElapsedTime();
+        motor = hardwareMap.get(DcMotor.class, "motorTest");
+        motor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        waitForStart();
+        timer.reset();
+        while (opModeIsActive() && (timer.seconds() < 15))
+        {
+            int motorZero = motor.getCurrentPosition();
+            motor.setTargetPosition(motorZero + 500);
+            motor.setPower(0.25);
+            while (motor.isBusy() && opModeIsActive())
+            {
+                telemetry.addData("expected", motorZero);
+                telemetry.addData("actual", motor.getCurrentPosition());
+                telemetry.addData("distance", Math.abs((motorZero + 500) - motor.getCurrentPosition()));
+                telemetry.update();
+            }
+            sleepTimer.reset();
+            while (sleepTimer.milliseconds() < 5000 && opModeIsActive())
+                idle();
+        }
+        while (opModeIsActive())
+            idle();
+    }
 }
+
