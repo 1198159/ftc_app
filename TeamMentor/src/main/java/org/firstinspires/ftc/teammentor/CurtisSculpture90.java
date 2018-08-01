64a1f9d364af48426a718d19883e2924d2e936e2
AM\geffners <steve.geffner.phd@gmail.com>
2018-07-31 18:39:18 -0700

    some quick and dirty motor power files for a sculpture project.

---
+++ TeamMentor/src/main/java/org/firstinspires/ftc/teammentor/CurtisSculpture90.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teammentor;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/**
+ * This file contains an opmode that tests whether an encoder is working.
+ */
+
+@TeleOp(name="90", group="Curtis")  // @Autonomous(...) is the other common choice
+//@Disabled
+public class CurtisSculpture90 extends LinearOpMode {
+
+    /* Declare OpMode members. */
+    DcMotor motor = null;
+    //DcMotor rightMotor = null;
+
+    @Override
+    public void runOpMode() {
+
+        motor  = hardwareMap.dcMotor.get("motor");
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+
+        motor.setPower(0.90);
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            idle();
+
+        }
+    }
+}
