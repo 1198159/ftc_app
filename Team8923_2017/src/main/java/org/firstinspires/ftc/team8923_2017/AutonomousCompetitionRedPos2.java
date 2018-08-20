d402d883fff9bd12ea926c632c351db2ff5627ed
Cole Welch <colewelch151@gmail.com>
2018-08-19 20:03:51 -0700

    ?  Fixed capital U in telemetry.update() that was causing a build error.

--- Team8923_2017/src/main/java/org/firstinspires/ftc/team8923_2017/AutonomousCompetitionRedPos2.java
+++ Team8923_2017/src/main/java/org/firstinspires/ftc/team8923_2017/AutonomousCompetitionRedPos2.java
@@ -23,7 +23,7 @@ public class AutonomousCompetitionRedPos2 extends MasterAutonomous
 
         telemetry.addData("InitState:", "InitStarted");
         telemetry.update();
-        telemetry.Update();
+        telemetry.update();
         InitAuto();//Initializes Hardware and sets position based on alliance
         initVuforia();//Initializes Vuforia
         telemetry.addData("InitState:", "InitFinished");
