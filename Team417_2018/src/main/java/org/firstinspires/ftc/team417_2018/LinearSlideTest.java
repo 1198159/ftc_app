19882952d6b12291c988002bb243530b64c99ae8
Madeline Nguyen <maddynnguyen@gmail.com>
2018-10-14 14:14:39 -0700

    Added Linear slide test to test last year's linear slides

---
+++ Team417_2018/src/main/java/org/firstinspires/ftc/team417_2018/LinearSlideTest.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.team417_2018;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Linear Slide Test")
+public class LinearSlideTest extends MasterTeleOp
+{
+    public void runOpMode() throws InterruptedException
+    {
+        super.initializeHardware();
+
+        waitForStart();
+        while (opModeIsActive())
+        {
+            runManualLift();
+            telemetry.addData("hi","there");
+            telemetry.update();
+        }
+    }
+}
