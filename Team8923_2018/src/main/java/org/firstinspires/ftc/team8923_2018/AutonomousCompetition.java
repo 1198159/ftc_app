ec31897459ad55ac2c5246365e8546473edfe292
Coleson Oliver <thenorthernquasar@gmail.com>
2018-10-23 21:05:23 -0700

    some testing info and telemetry

--- Team8923_2018/src/main/java/org/firstinspires/ftc/team8923_2018/AutonomousCompetition.java
+++ Team8923_2018/src/main/java/org/firstinspires/ftc/team8923_2018/AutonomousCompetition.java
@@ -17,41 +17,19 @@ public class AutonomousCompetition extends MasterAutonomous
     @Override
     public void runOpMode() throws InterruptedException
     {
-        //configureAutonomous();
+        configureAutonomous();
         initAuto();
 
         waitForStart();
 
-        motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //moveLift(500);
 
-        motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        motorBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        moveLift(500);
-        /*
-        motorFL.setPower(0.25);
-        motorFR.setPower(0.25);
-        motorBL.setPower(0.25);
-        motorBR.setPower(0.25);
-        */
+        telemetry.clear();
+        telemetry.update();
 
         while (opModeIsActive())
         {
-            telemetry.addData("FL power", motorFL.getPower());
-            telemetry.addData("FR power", motorFR.getPower());
-            telemetry.addData("BL power", motorBL.getPower());
-            telemetry.addData("BR power", motorBR.getPower());
-            telemetry.addData("FL encoder", motorFL.getCurrentPosition());
-            telemetry.addData("FR encoder", motorFR.getCurrentPosition());
-            telemetry.addData("BL encoder", motorBL.getCurrentPosition());
-            telemetry.addData("BR encoder", motorBR.getCurrentPosition());
-            telemetry.update();
-            //driveToPoint(0,1000, 0, 1.0);
+            driveToPoint(0,500, 0, 1.0);
             idle();
         }
 
