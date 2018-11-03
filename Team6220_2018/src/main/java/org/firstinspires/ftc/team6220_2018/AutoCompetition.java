6d6f147e08af086d803b30841bd7757d9e496ea5
Cole Welch <colewelch151@gmail.com>
2018-11-03 14:54:57 -0700

    Revert "Added code for red / blue depot autonomous routines.  *Needs to be tested and have constants adjusted."
    This reverts commit 5a6bee8cb73319101c706d82fdb14e4ffcaa7d8a.

--- Team6220_2018/src/main/java/org/firstinspires/ftc/team6220_2018/AutoCompetition.java
+++ Team6220_2018/src/main/java/org/firstinspires/ftc/team6220_2018/AutoCompetition.java
@@ -41,7 +41,6 @@ public class AutoCompetition extends MasterAutonomous
                 servoHanger.setPosition(Constants.SERVO_HANG_RETRACTED);
                 motorHanger.setPower(0);
                 pauseWhileUpdating(1.5);
-
                 // Turn and detect minerals
                 identifyGold();
 
@@ -74,7 +73,7 @@ public class AutoCompetition extends MasterAutonomous
                 // Drive forward
                 driveToPosition(0,1100,0.7);
 
-                // moveRobot forward quickly, moveRobot backward quickly to drop team marker
+                // moveRobot forward quickly, moveRobot backward quickly
                 moveRobot(90,1.0,0.2);
                 moveRobot(-90,1.0,0.2);
 
@@ -91,7 +90,6 @@ public class AutoCompetition extends MasterAutonomous
                 servoHanger.setPosition(Constants.SERVO_HANG_RETRACTED);
                 motorHanger.setPower(0);
                 pauseWhileUpdating(1.5);
-
                 // Turn and detect minerals
                 identifyGold();
 
@@ -106,21 +104,21 @@ public class AutoCompetition extends MasterAutonomous
                 // Drive forward and knock off correct mineral
                 knockGold(goldLocation);
 
-                // Turn robot toward depot based on gold mineral position
-                if (goldLocation == sampleFieldLocations.left)
-                    turnTo(-30,0.7);
-                else if (goldLocation == sampleFieldLocations.right)
-                    turnTo(30,0.7);
+                // Drive forward; we change this value based on where the gold mineral was
+                driveToPosition(0,1250 + mineralShift,0.7);
 
-                // Drive forward into depot
-                driveToPosition(0,850,0.7);
+                // Turn 45 deg ccw
+                turnTo(135,1.0);
 
-                // moveRobot forward quickly, moveRobot backward quickly to drop team marker
+                // Align robot with wall
+                moveRobot(0.0,0.6,0.8);
+
+                // Drive forward
+                driveToPosition(0,1100,0.7);
+
+                // moveRobot forward quickly, moveRobot backward quickly
                 moveRobot(90,1.0,0.2);
                 moveRobot(-90,1.0,0.2);
-
-                // Orient robot with rear toward crater
-                turnTo(-45,1.0);
 
                 // Drive backward into crater
                 driveToPosition(0,-1850,1.0);
@@ -139,7 +137,6 @@ public class AutoCompetition extends MasterAutonomous
                 servoHanger.setPosition(Constants.SERVO_HANG_RETRACTED);
                 motorHanger.setPower(0);
                 pauseWhileUpdating(1.5);
-
                 // Turn and detect minerals
                 identifyGold();
 
@@ -172,7 +169,7 @@ public class AutoCompetition extends MasterAutonomous
                 // Drive forward
                 driveToPosition(0,1100,0.7);
 
-                // moveRobot forward quickly, moveRobot backward quickly to drop team marker
+                // moveRobot forward quickly, moveRobot backward quickly
                 moveRobot(90,1.0,0.2);
                 moveRobot(-90,1.0,0.2);
 
@@ -182,46 +179,7 @@ public class AutoCompetition extends MasterAutonomous
             // Red + Depot-----------------------------------------------------------------------
             else
             {
-                // Drop robot to ground
-                motorHanger.setTargetPosition(Constants.HANG_UNLATCH_POSITION);
-                motorHanger.setPower(1.0);
-                pauseWhileUpdating(0.3);
-                servoHanger.setPosition(Constants.SERVO_HANG_RETRACTED);
-                motorHanger.setPower(0);
-                pauseWhileUpdating(1.5);
 
-                // Turn and detect minerals
-                identifyGold();
-
-                // Unlatch from hook while on ground
-                motorHanger.setTargetPosition(Constants.HANG_GROUND_UNLATCH);
-                motorHanger.setPower(1.0);
-                driveToPosition(60,0,0.7);
-                motorHanger.setTargetPosition(0);
-                motorHanger.setPower(1.0);
-                driveToPosition(-60,0,0.7);
-
-                // Drive forward and knock off correct mineral
-                knockGold(goldLocation);
-
-                // Turn robot toward depot based on gold mineral position
-                if (goldLocation == sampleFieldLocations.left)
-                    turnTo(-30,0.7);
-                else if (goldLocation == sampleFieldLocations.right)
-                    turnTo(30,0.7);
-
-                // Drive forward into depot
-                driveToPosition(0,850,0.7);
-
-                // moveRobot forward quickly, moveRobot backward quickly to drop team marker
-                moveRobot(90,1.0,0.2);
-                moveRobot(-90,1.0,0.2);
-
-                // Orient robot with rear toward crater
-                turnTo(-45,1.0);
-
-                // Drive backward into crater
-                driveToPosition(0,-1850,1.0);
             }
 
         }
