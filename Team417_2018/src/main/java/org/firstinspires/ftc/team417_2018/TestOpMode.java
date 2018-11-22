5a39d118fe03da8cd62eb7fdb1dd13d455e22c61
Madeline Nguyen <maddynnguyen@gmail.com>
2018-11-22 11:13:09 -0800

    Tested move() method deadreckoning

--- Team417_2018/src/main/java/org/firstinspires/ftc/team417_2018/TestOpMode.java
+++ Team417_2018/src/main/java/org/firstinspires/ftc/team417_2018/TestOpMode.java
@@ -3,41 +3,21 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous(name = "TestOpModeTest")
+@Autonomous(name = "TestOpMode")
 //@Disabled
-public class TestOpMode extends LinearOpMode {
-
-    private DcMotor FrontLeftMotor;
-    private DcMotor BackRightMotor;
-    private DcMotor BackLeftMotor;
-    private DcMotor FrontRightMotor;
-
-    @Override
-
+public class TestOpMode extends MasterAutonomous {
 
     public void runOpMode() throws InterruptedException
     {
-        FrontLeftMotor = hardwareMap.dcMotor.get("FrontLeft");
-        FrontRightMotor = hardwareMap.dcMotor.get("FrontRight");
-        BackLeftMotor = hardwareMap.dcMotor.get("BackLeft");
-        BackRightMotor = hardwareMap.dcMotor.get("BackRight");
-
-        FrontLeftMotor.setDirection(DcMotor.Direction.REVERSE);
-        BackLeftMotor.setDirection(DcMotor.Direction.REVERSE);
+        super.initializeHardware();
 
         waitForStart();
-
-        FrontLeftMotor.setPower(0.25);
-        FrontRightMotor.setPower(0.25);
-        BackRightMotor.setPower(0.25);
-        BackLeftMotor.setPower(0.25);
-
-
-        sleep(3000);
-
-        FrontLeftMotor.setPower(0);
-        FrontRightMotor.setPower(0);
-        BackRightMotor.setPower(0);
-        BackLeftMotor.setPower(0);
+        move(200, 0, 0.3, 0.7, 1.0);
+        sleep(200);
+        move(0, 200, 0.3, 0.7, 1.0);
+        sleep(200);
+        move(-200, 0, 0.3, 0.7, 1.0);
+        sleep(200);
+        move(0, -200, 0.3, 0.7, 1.0);
     }
 }
