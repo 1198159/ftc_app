84a03cf665e8dd8cd29538e583fc6f1373e1310b
Madeline Nguyen <maddynnguyen@gmail.com>
2019-02-05 17:57:46 -0800

    tested Steve's quarter scale servo

--- Team417_2018/src/main/java/org/firstinspires/ftc/team417_2018/LinearSlideTest.java
+++ Team417_2018/src/main/java/org/firstinspires/ftc/team417_2018/LinearSlideTest.java
@@ -3,37 +3,28 @@ package org.firstinspires.ftc.team417_2018;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "Linear Slide Test")
 //@Disabled
 public class LinearSlideTest extends MasterTeleOp
 {
     int targetPos = 0;
+    Servo rev1 = null; // hub 2 port 2
 
     public void runOpMode() throws InterruptedException
     {
-        super.initializeHardware();
-        core2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // set encoder value to 0
-        core2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        //super.initializeHardware();
+
+        rev1 = hardwareMap.servo.get("rev1");
+        rev1.setPosition(0.5);
         telemetry.addData("Init","done");
         telemetry.update();
 
         waitForStart();
         while (opModeIsActive())
         {
-            if (gamepad2.right_trigger != 0 && targetPos < MAX_CORE_POS)
-            {
-                targetPos++;
-                core2.setTargetPosition(targetPos);
-                core2.setPower(0.7);
-            }
-            if (gamepad2.left_trigger != 0 && targetPos > -50)
-            {
-                targetPos--;
-                core2.setTargetPosition(targetPos);
-                core2.setPower(0.8);
-            }
-
+            rev1.setPosition(0.5);
             telemetry.addData("tarPos", targetPos);
             telemetry.update();
         }
